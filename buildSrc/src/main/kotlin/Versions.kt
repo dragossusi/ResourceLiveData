20f7c97a47a7cfac6e4907403dc32916eb48d0db
Dragos Rachieru <rachierudragos97@gmail.com>
2021-02-18 16:49:01 +0200

    Updated versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -1,5 +1,5 @@
 object Versions {
-    const val kotlin = "1.4.21"
-    const val lifecycle_version = "2.2.0"
-    const val app = "1.0.2"
+    const val kotlin = "1.4.30"
+    const val lifecycle_version = "2.3.0"
+    const val app = "1.0.4"
 }
